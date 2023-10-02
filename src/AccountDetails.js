35c26933468c5e77830dc8f2f88f8fcc364cad47
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-10-02 18:29:02 +0200

    added token balance to account details component

--- src/AccountDetails.js
+++ src/AccountDetails.js
@@ -11,23 +11,26 @@ const settings = {
 const alchemy = new Alchemy(settings);
 
 function AccountDetails() {
-  const { address: originalAddress } = useParams();
-  const [address, setAddress] = useState(originalAddress);
+    const { address: originalAddress } = useParams();
+    const [address, setAddress] = useState(originalAddress);
   const [balance, setBalance] = useState(null);
-  const history = useHistory();
+  const [tokenBalances, setTokenBalances] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
+  const history = useHistory();
 
   useEffect(() => {
     async function fetchAccountDetails() {
       // Fetch account details using the address parameter
       const accountBalance = await alchemy.core.getBalance(address);
-
-      // Convert Wei to Ether
       const balanceInEther = accountBalance && typeof accountBalance === 'object' && '_hex' in accountBalance && '_isBigNumber' in accountBalance
         ? Utils.formatEther(accountBalance)
         : String(accountBalance);
 
       setBalance(balanceInEther);
+
+      // Fetch token balances using the address parameter
+      const tokens = (await alchemy.core.getTokenBalances(address)).tokenBalances;
+      setTokenBalances(tokens);
     }
 
     fetchAccountDetails();
@@ -37,22 +40,18 @@ function AccountDetails() {
     history.goBack();
   };
 
-  const clearSearch = () => {
-    setSearchTerm('');
-    // Reset the address to the original one
-    setAddress(originalAddress);
-  };
-
   const handleSearch = async () => {
     // Perform the search logic and obtain the new address from the search term
     // For simplicity, we assume the search term is the new address
     const newAddress = searchTerm;
 
-    // Update the address state to trigger a re-fetch of account details
-    setAddress(newAddress);
+    // Update the address state only if the search term is not empty
+    if (newAddress.trim() !== '') {
+      setAddress(newAddress);
+    }
   };
 
-  if (!balance) {
+  if (!balance || !tokenBalances) {
     return <div>Loading...</div>;
   }
 
@@ -64,21 +63,38 @@ function AccountDetails() {
       <button className="homeButton" onClick={() => history.push('/')}>
         Go to Homepage
       </button>
-      <h2>Account Details - {address}</h2>
+      <h2>Account Details</h2>
+      <p>Balance: {balance} Ether</p>
+
+      {/* Search bar */}
       <div>
-        {/* Search bar */}
         <input
           type="text"
-          placeholder="Search by address"
+          placeholder="Search by Contract Address"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        {/* Search button */}
         <button onClick={handleSearch}>Search</button>
-        {/* Clear button */}
-        <button onClick={clearSearch}>Clear Search</button>
       </div>
-      <p>Balance: {balance} Ether</p>
+
+      <h3>Token Balances:</h3>
+      <table className="tokenBalancesTable">
+        <thead>
+          <tr>
+            <th>Contract Address</th>
+            <th>Token Balance (Ether)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tokenBalances.map((token, index) => (
+            <tr key={index}>
+              <td>{token.contractAddress}</td>
+              <td>{Utils.formatUnits(token.tokenBalance, token.decimals)} Ether</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       {/* Display other account details as needed */}
     </div>
   );
